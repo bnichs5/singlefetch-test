3bb5e9c4681eed2c058eecfb1c1529f0d75cb119
bnichs5 <bnichs51259@gmail.com>
2022-03-07 22:36:49 -0500

    Update main.go

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ const (
 
 func main() {
 	proxyUrl := host() + ":" + port()
-	now := time.Now()
+	//now := time.Now()
 	
 	http.HandleFunc("/favicon.ico", func(rw http.ResponseWriter, r *http.Request) {
 		favicon(rw)
